586be4e08c1c956c9cf76f05d5b59e86f5ac6ea0
Karolis Zelvys <koralbish@gmail.com>
2024-02-01 21:55:07 +0200

    Fixed studio-update

--- ReviewTBD/Controllers/StudioController.cs
+++ ReviewTBD/Controllers/StudioController.cs
@@ -40,7 +40,7 @@ public class StudioController(IStudioService studioService) : ControllerBase {
 
     }
 
-    [HttpPut("Update/{id:guid}")]
+    [HttpPut("{id:guid}")]
     public async Task<ActionResult<StudioDto>> UpdateStudio(Guid id, [FromBody] StudioDto input) {
 
         var updated = await studioService.UpdateStudioAsync(id, input);
